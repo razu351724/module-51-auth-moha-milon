0e1bd52a5951b27ed85617e6d82de633d5b79722
RAZU-sheikh <razu351724@gmail.com>
2023-10-02 07:29:59 +0600

    Login section and Register section page tilwine and desul use and style satap

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 const Home = () => {
     return (
-        <div>
+        <div className="container mx-auto">
             <h2 className="text-3xl">This is home</h2>
         </div>
     );
--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -3,7 +3,7 @@ import Navbar from "./Navbar";
 
 const Root = () => {
     return (
-        <div>
+        <div  className="container mx-auto">
             <Navbar></Navbar>
             <Outlet></Outlet>
         </div>
